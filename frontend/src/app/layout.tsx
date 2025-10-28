e3568d2931c09b83b8dcf846d56ccb0f184ad148
Dmitry <starodu5@gmail.com>
2025-10-27 22:36:34 -0400

    scrollbar

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-// import "../styles/styles.css";
+import "../styles/styles.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
